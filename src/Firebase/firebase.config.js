98e75b0cf2a31f848227bc1095ff589cd7627c28
Sofiullah01 <ssisofiulislam@gmail.com>
2023-11-29 16:24:25 +0600

    add auth provider

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -2,12 +2,12 @@ import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyASg2-x2fryTM467WVIXL6yA2Z8Nb0Q8T8",
-  authDomain: "email-pass-project-b7e9f.firebaseapp.com",
-  projectId: "email-pass-project-b7e9f",
-  storageBucket: "email-pass-project-b7e9f.appspot.com",
-  messagingSenderId: "997938588541",
-  appId: "1:997938588541:web:c77354b236927fb01b3a14"
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './MainRoure/MainRouter'
-import { Toaster } from 'react-hot-toast'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./MainRoure/MainRouter";
+import { Toaster } from "react-hot-toast";
+import AuthProvider from "./Provider/AuthProvider/AuthProvider";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div className="max-w-6xl mx-auto">
-    <RouterProvider router={router} />
-    <Toaster/>
-    </div>
-  </React.StrictMode>,
-)
+    <AuthProvider>
+      <div className="max-w-6xl mx-auto">
+        <RouterProvider router={router} />
+        <Toaster />
+      </div>
+    </AuthProvider>
+  </React.StrictMode>
+);
